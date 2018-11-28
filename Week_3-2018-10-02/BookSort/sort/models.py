b1eff1674d293dda6abd54efb42f0b85ca346e8f
valinma <mawei01@hotmail.com>
2018-11-28 17:53:40 -0500

    Update

---
+++ Week_3-2018-10-02/BookSort/sort/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Book(models.Model):
+	link = models.CharField(max_length=100,null=False)
+	title = models.CharField(max_length=255,null=False)
+	img_url = models.CharField(max_length=255)
+	author = models.CharField(max_length=50)
+	rating = models.FloatField(max_length=5,null=False)
+	ratingsCount = models.IntegerField(null=False)
+	genre_type = models.CharField(max_length=20,null=True)
+	publish_year = models.IntegerField(null=False)
+	description = models.TextField()
+
+	def __str__(self):
+		return self.title
